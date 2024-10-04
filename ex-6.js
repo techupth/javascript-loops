ea119cf9332ff1b93f0b9162877d0f703b9d94aa
FSD7-Boy <boyzasad01@gmail.com>
2024-10-04 13:43:01 +0700

    feat: assignment javascript loop

--- ex-6.js
+++ ex-6.js
@@ -4,4 +4,8 @@ let reversedCompanyName = "";
 
 // Start coding here
 
+for (let i = 0; i < companyName.length; i++) {
+  reversedCompanyName = companyName[i] + reversedCompanyName;  
+}
+
 console.log(reversedCompanyName);
